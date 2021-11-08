286753578ea373ddb5eed2334123350c255baecd
arnold2100 <93554113+arnold2100@users.noreply.github.com>
2021-11-08 16:30:52 -0400

    Add files via upload

---
+++ form.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>formulaario</h1>
+    <form>
+        <input value="nombre" name="nombre" type="text"/><br>
+        <input value="numero" name="numero" type="text"/><br>
+        <input value="email" name="email" type="email"/><br>
+        <textarea name="textarea"></textarea>
+        <p>numero</p>
+    <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+    </select>
+    </form>
+    <br>
+    <table>
+        <tr>
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+        </tr>
+    </table>
+</body>
+</html>
